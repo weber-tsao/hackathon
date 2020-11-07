f14c0bcb21e94d2cbe22c0d7cc2ebc48c798e49a
serfinxx <74106382+serfinxx@users.noreply.github.com>
2020-11-07 22:53:14 +0000

    timer name changed

--- js/javascript1.js
+++ js/javascript1.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // timer function
-document.getElementById("starttimer").addEventListener("click", function(){
+document.getElementById("startTimer").addEventListener("click", function(){
   var timeleft = 60;
   var downloadTimer = setInterval(function function1(){
   document.getElementById("countdown").innerHTML = timeleft + 
